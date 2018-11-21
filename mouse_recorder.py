5f828cc252a6344c0f734d0658a584e787d8d919
eduardosm7 <esm7@cin.ufpe.br>
2018-11-21 17:51:07 -0300

    Add files

---
+++ mouse_recorder.py
@@ -0,0 +1,74 @@
+"""
+Mouse recorder
+"""
+
+import sys
+import time
+from pynput.mouse import Controller
+
+# Instanciates mouse controller object
+mouse = Controller()
+
+# List holding tuples <tuple(x_pos, y_pos), float(delay_time)>
+positions = []
+
+
+def record(duration):
+    """
+    Records user mouse input
+    """
+
+    pos = mouse.position
+
+    time_zero = time.time()
+    time_last = time_zero
+
+    while time.time() - time_zero < duration:
+
+        # Only records if mouse position changed
+        if mouse.position != pos:
+
+            pos = mouse.position
+            delay_time = time.time() - time_last
+
+            # Feeds tuple to the positions list
+            positions.append((pos, delay_time))
+
+            # Resets timer
+            time_last = time.time()
+        
+
+def replay():
+    """
+    Replays user recorded mouse input
+    """
+    
+    for tup in positions:
+
+        # Sets mouse position
+        mouse.position = tup[0]
+
+        # Sleeps the delay time
+        time.sleep(tup[1])
+
+
+def main():
+    
+    try:
+        duration = float(input("Input the duration of the recording (in seconds): ").replace(',', '.'))
+    except ValueError:
+        print("Please input a valid time in seconds.")
+        sys.exit(1)
+
+    input("Press any key to start recording...")
+    print("Recording...")
+
+    record(duration)
+    
+    input("Press any key to replay...")
+
+    replay()
+
+
+if __name__ == "__main__":
+    main()
